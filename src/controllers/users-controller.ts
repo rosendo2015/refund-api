332e354be69f8c777aec8bb304a7ab9e89a0ca20
rosendo <rosendc30@gmail.com>
2026-03-19 09:01:46 -0300

    prisma 7 e validação de dados usuário

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,17 @@
 import { Request, Response } from "express"
+import { UserRule } from "@/generated/prisma/client"
+import z from "zod"
 
 class UsersController {
     async create(request: Request, response: Response) {
-        return response.json({ message: "Usuário criado com sucesso" })
+        const bodyschema = z.object({
+            name: z.string().trim().min(3, { message: "Nome é obrigatório" }),
+            email: z.string().trim().email({ message: "Email inválido" }).toLowerCase(),
+            password: z.string().min(6, { message: "A senha de pelo menos 6 números" }),
+            role: z.enum([UserRule.employee, UserRule.manager]).default(UserRule.employee)
+        })
+        const { name, email, password, role } = bodyschema.parse(request.body)
+        return response.json({ name, email, password, role })
     }
 }
 export { UsersController }
--- src/database/prisma.ts
+++ src/database/prisma.ts
@@ -1,5 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg"
 
-export const prisma = new PrismaClient({
-    log: ["query"]
+const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL!
 })
+
+export const prisma = new PrismaClient({ adapter })
